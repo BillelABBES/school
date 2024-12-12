c85e44dee51dd7573de831fa4ab03e43791ea72b
Billel ABBES <ba.thetiptop@gmail.com>
2024-12-12 15:45:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/User');
 const courseRoutes = require('./routes/course');
 
 const app = express();
